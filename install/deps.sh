671cf211c73d0a0e413dfe05d3025a03d95396c3
jspc <james@zero-internet.org.uk>
2013-02-16 23:38:59 +0000

    Sep out

--- install/deps.sh
+++ install/deps.sh
@@ -44,22 +44,4 @@ for APP in wget readline emacs; do
     $INSTALL_CMD $APP 2>/dev/null
 done
 
-__msg "Installing rvm:"
-curl -L https://get.rvm.io | bash -s stable --ruby 2>/dev/null
-
-__msg "Installing ruby 1.9.3-p385"
-[ -f $HOME/.rvm/scripts/rvm ]     && source $HOME/.rvm/scripts/rvm
-[ -f /usr/local/rvm/scripts/rvm ] && source /usr/local/rvm/scripts/rvm
-
-rvm install 1.9.3-p385
-rvm use 1.9.3-p385
-
-__msg "Installing perlbrew"
-curl -kL http://install.perlbrew.pl | bash
-
-__msg "Installing Perl 5.16.0"
-source ~/perl5/perlbrew/etc/bashrc
-perlbrew install perl-5.16.0
-perlbrew switch perl-5.16.0
-
 __msg "deps.sh done!\n"
